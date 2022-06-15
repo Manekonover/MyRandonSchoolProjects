9f9d66c0c32cdceae609be927dc5f3bfadfe55e6
Manekonover <100494920+Manekonover@users.noreply.github.com>
2022-06-15 11:41:31 -0400

    Create Going into circles with a machine.py

---
+++ Going into circles with a machine.py
@@ -0,0 +1,12 @@
+#machine that changes quordinates
+Mylist = [1, 1]
+print(Mylist)
+for i in range(3):
+    Mylist[0] += 4
+    print(Mylist)
+    Mylist[1] +=5
+    print(Mylist)
+    Mylist[0] -=4
+    print(Mylist)
+    Mylist[1] -=5
+    print(Mylist)
